25202be95eb4e753ba7c629e8d47a30856e6c49d
kmriazuddin <mohammadriazuddin3@gmail.com>
2025-02-05 02:39:12 +0600

    I have created 3 sections on the UI home page,which are top rated doctors card, why choose us and how it works etc.

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,17 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import HowItWorks from "@/components/UI/HomePage/HowItWorks/HowItWorks";
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUS from "@/components/UI/HomePage/WhyUS/WhyUS";
 
 const page = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
+      <WhyUS />
+      <HowItWorks />
     </>
   );
 };
